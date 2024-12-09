7ae17cbc5ccdc6fed47bcfc565893989d45e8f6a
Lucas Borja <lborja@LIVE.COM>
2024-12-09 18:13:58 -0300

    parte 5 - Angular, interpolação, diretiva e binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  
-  public eventos: any;
+  larguraImagem : number = 150;
+  margemImagem : number = 2;
+  mostrarImagem: boolean = true;
+  public eventos: any = [];
+  private _filtroLista : string = '';
+  public eventosFiltrados: any = [];
+
+  public get filtroLista():string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value:string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:{ tema: String; local:string}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
  
   constructor(private http: HttpClient) { }
@@ -20,9 +41,16 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:7063/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
+  }
+
+   AlterarTamanhoImagem (){
+    this.mostrarImagem = !this.mostrarImagem
   }
 
 }
